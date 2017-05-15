c18b861ce04c69de4157939b4a83195643d26e3f
Blair Hudson <blairhudson@me.com>
2017-05-15 21:02:37 +1000

    Fix packages listing and remove download URL

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 setup(
   name = 'partybus',
-  packages = ['partybus'],
+  packages = ['partybus',
+              'partybus.magic'],
   version = '0.1',
   description = 'Connecting #OpenData and Data Scientists',
   author = 'DataSciParty',
   author_email = 'datasciparty@gmail.com',
   url = 'https://github.com/DataSciParty/Party-Bus', 
-  download_url = 'https://github.com/DataSciParty/Party-Bus/archive/master.zip',
+  download_url = '',
   keywords = ['data', 'open', 'science'],
   classifiers = [],
 )
